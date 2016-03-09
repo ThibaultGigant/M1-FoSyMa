1dc3bf5a17ed05672702d095b35a64510f1a4604
Thibault Gigant <thibault.gigant@gmail.com>
2016-03-09 12:04:25 +0100

    Merge gestion DF pour send et receive behaviours

---
+++ DedaleEtuFull/src/mas/protocols/BlocageBehaviour.java
@@ -0,0 +1,28 @@
+package mas.protocols;
+
+import mas.abstractAgent;
+
+/**
+ * Created by Tigig on 09/03/2016.
+ */
+public class BlocageBehaviour extends AbstractProtocol {
+    /**
+     * Dernier protocole suivi par l'agent, afin qu'il puisse le retrouver après résolution du blocage
+     */
+    IProtocol lastProtocol;
+
+    /**
+     * Constructor
+     * @param lastProtocol
+     */
+    public BlocageBehaviour(IProtocol lastProtocol) {
+        this.lastProtocol = lastProtocol;
+    }
+
+    @Override
+    public void addBehaviours(abstractAgent myAgent) {
+        behaviours.add(new AskBlockerBehaviour(lastProtocol));
+
+        behaviours.forEach(myAgent::addBehaviour);
+    }
+}
